4a8862c1e37d0bc39d7e83a3b91101bcbe328a2a
claudia-gp <claudia.gonzalez@mail.polimi.it>
2015-12-03 18:15:32 +0100

    Added sprites and scripts from my old project

---
+++ ObliviousOscar/Assets/Scripts/Switch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : Tappable {
+
+	private Rigidbody2D rb;
+	public GameObject doorToOpen;
+	
+	void Start () {
+		base.Start ();
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	public override void OnTap ()
+	{
+		BoxCollider2D bc = doorToOpen.GetComponent<BoxCollider2D>();
+		//doorToOpen.animator.
+		Destroy (bc);
+
+	}
+}

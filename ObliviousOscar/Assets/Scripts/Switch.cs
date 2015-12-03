e01a97a1951374c3f4bfed970aef8706360b5b34
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2015-12-03 18:19:15 +0100

    Refactoring

--- ObliviousOscar/Assets/Scripts/Switch.cs
+++ ObliviousOscar/Assets/Scripts/Switch.cs
@@ -1,21 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Switch : Tappable {
+public class Switch : Tappable
+{
 
 	private Rigidbody2D rb;
 	public GameObject doorToOpen;
 	
-	void Start () {
+	void Start ()
+	{
 		base.Start ();
 		rb = GetComponent<Rigidbody2D> ();
 	}
 
 	public override void OnTap ()
 	{
-		BoxCollider2D bc = doorToOpen.GetComponent<BoxCollider2D>();
-		//doorToOpen.animator.
+		BoxCollider2D bc = doorToOpen.GetComponent<BoxCollider2D> ();
 		Destroy (bc);
-
 	}
 }

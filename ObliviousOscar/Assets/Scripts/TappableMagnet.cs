f07671686ade2e74416303c0c2e86797cab45a9c
pedrogm <pekeott@gmail.com>
2015-12-09 13:56:18 +0100

    added magnet prefab, 2D rope system and some sprites

---
+++ ObliviousOscar/Assets/Scripts/TappableMagnet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TappableMagnet : Tappable
+{
+	DistanceJoint2D dj;
+	public override void OnTap ()
+	{
+
+		Debug.Log("asdfd");
+		dj = GetComponent<DistanceJoint2D> ();
+		dj.distance = 0;
+
+
+	}
+	
+}

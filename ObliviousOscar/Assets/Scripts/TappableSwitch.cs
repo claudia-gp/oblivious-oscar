c663f3f88abae74d2580375571d05202f28f9810
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2015-12-10 13:43:24 +0100

    Added switch sprite change when activated

--- ObliviousOscar/Assets/Scripts/TappableSwitch.cs
+++ ObliviousOscar/Assets/Scripts/TappableSwitch.cs
@@ -6,8 +6,14 @@ public class TappableSwitch : Tappable
 
 	public GameObject doorToOpen;
 
+	public Sprite openDoorSprite;
+
+	public Sprite tappedSwitchSprite;
+
 	public override void OnTap ()
 	{
+		GetComponent<SpriteRenderer> ().sprite = tappedSwitchSprite;
+		doorToOpen.GetComponent<SpriteRenderer> ().sprite = openDoorSprite;
 		Destroy (doorToOpen.GetComponent<BoxCollider2D> ());
 	}
 }

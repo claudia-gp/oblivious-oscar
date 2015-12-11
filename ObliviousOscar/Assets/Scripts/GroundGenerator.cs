f0e0c99711ecdce0c1b99fc23b3e52a30ccf2961
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2015-12-11 01:57:52 +0100

    Added GroundGenerator editor script

---
+++ ObliviousOscar/Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,48 @@
+﻿#if UNITY_EDITOR	
+
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class GroundGenerator : MonoBehaviour
+{
+	public bool generateGround = false;
+	public Transform groundContainer;
+	public GameObject groundPrefab;
+
+	static int c = 0;
+
+	void Update ()
+	{
+		Debug.Log ("I was updated " + c++);
+		if (generateGround) {
+			generateGround = false;
+
+			Transform rightMost = null;
+
+			foreach (var t in groundContainer.GetComponentsInChildren<Transform> ()) {
+				if (rightMost == null || t.position.x > rightMost.position.x) {
+					rightMost = t;
+				}
+			}
+
+			Debug.Log ("Rightmost x : " + rightMost.transform.position.x);
+			var generated = PrefabUtility.InstantiatePrefab (groundPrefab) as GameObject;
+			generated.transform.SetParent (groundContainer);
+			generated.transform.position = new Vector3 (rightMost.position.x + Offset (), groundContainer.position.y);
+		}
+	}
+
+	float Offset ()
+	{
+		return groundPrefab.GetComponent<SpriteRenderer> ().sprite.bounds.extents.x * 2;
+	}
+}
+
+#else
+
+public class GroundGenerator : UnityEngine.MonoBehaviour
+{
+}
+
+#endif

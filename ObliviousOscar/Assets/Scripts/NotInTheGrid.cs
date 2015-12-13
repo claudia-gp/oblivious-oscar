1f4bffb4be69ab3dc10a9c461a53fcd916308b3f
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2015-12-13 20:41:55 +0100

    Updated Tutorial

--- ObliviousOscar/Assets/Scripts/NotInTheGrid.cs
+++ ObliviousOscar/Assets/Scripts/NotInTheGrid.cs
@@ -1,7 +1,4 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class NotInTheGrid : MonoBehaviour
+﻿public class NotInTheGrid : UnityEngine.MonoBehaviour
 {
 	//Empty: this is used just to recognize the object
 }

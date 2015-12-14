844cece951c56e06d5a5d9065337bd685eb18731
claudia-gp <claudia.gonzalez@mail.polimi.it>
2015-12-14 12:45:33 +0100

    Updated elevator trap and level scene

--- ObliviousOscar/Assets/Scripts/Tappable/TappableMagnet.cs
+++ ObliviousOscar/Assets/Scripts/Tappable/TappableMagnet.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class TappableMagnet : Tappable
 {
+	private bool isActivated = false;
+
 	public override void OnClick ()
 	{
-		GetComponent<DistanceJoint2D> ().distance = 0;
+		if (isActivated == false) {
+			GetComponent<DistanceJoint2D> ().distance = 0;
+			isActivated = true;
+		} else {
+			GetComponent<DistanceJoint2D> ().distance = 3.4f;
+			isActivated = false;
+		}
 	}
 	
 }

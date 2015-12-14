c17dc9db08831fb795a7f37f4e9d1e668e37366e
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2015-12-14 23:04:58 +0100

    Updated tutorial and Oscar speed, added UndergroundFiller editor script

--- ObliviousOscar/Assets/Scripts/Editor Scripts/UndergroundFiller.cs
+++ ObliviousOscar/Assets/Scripts/Editor Scripts/UndergroundFiller.cs
@@ -1,7 +1,26 @@
 ﻿using UnityEngine;
-using UnityEditor;
 
+[ExecuteInEditMode]
 public class UndergroundFiller : MonoBehaviour
 {
+	public bool createBlocks;
+	public GameObject other;
 	
+	void Update ()
+	{
+		if (createBlocks) {
+			createBlocks = false;
+			GameObject obj1 = gameObject;
+			GameObject obj2 = other;
+			GameObject newObj;
+
+			for (float i = obj1.transform.position.x; i < obj2.transform.position.x + 1; i++) {
+				for (float j = obj1.transform.position.y; j < obj2.transform.position.y + 1; j++) {
+					newObj = Instantiate (obj2, new Vector3 (i, j), Quaternion.identity) as GameObject;
+					newObj.transform.SetParent (obj2.transform.parent);
+				}
+			}
+			Destroy (newObj);
+		}
+	}
 }

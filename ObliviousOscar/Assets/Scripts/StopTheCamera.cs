d771bf2da0431bcd75e9810e7fbe23f448a7c3d9
claudia-gp <claudia.gonzalez@mail.polimi.it>
2015-12-14 23:59:20 +0100

    Updated the level and made EndOfLevel prefabs

---
+++ ObliviousOscar/Assets/Scripts/StopTheCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopTheCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D oscar){
+		if(oscar.tag.Equals(Oscar.Tag)){
+			Camera.main.transform.parent = null;			
+		}
+	}
+}

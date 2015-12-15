b1f1807b4961e0934e93dc5990e02f4f1d013da1
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2015-12-15 10:40:58 +0100

    Added Sun, changed SavePointsManager

--- ObliviousOscar/Assets/Scripts/SavePointsManager.cs
+++ ObliviousOscar/Assets/Scripts/SavePointsManager.cs
@@ -3,8 +3,18 @@ using System.Collections;
 
 public class SavePointsManager : PersistentSingleton<SavePointsManager>
 {
+	//TODO: could be changed with array
+	static readonly Hashtable positions = new Hashtable ();
+			
+	public Vector3 LatestPosition { 
+		get {
+			return (Vector3)positions [Application.loadedLevel];
+		}
+		set {
+			positions [Application.loadedLevel] = value;
+		}
+	}
 
-	public Vector3 LatestPosition {	get; set; }
 
 	void Start ()
 	{

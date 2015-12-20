758a2224144e6411e1d779aacc20052334629b74
pedrogm <pekeott@gmail.com>
2015-12-20 18:26:40 +0100

    added tappable door script and prefab, new test scene

---
+++ ObliviousOscar/Assets/Scripts/Tappable/TappableDoor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TappableDoor : MonoBehaviour
+{
+	
+
+	public Sprite openDoorSprite;
+
+	
+	public void OnClick ()
+	{
+		GetComponent<SpriteRenderer> ().sprite = openDoorSprite;
+		Destroy (GetComponent<BoxCollider2D> ());
+		Oscar.Instance.IsRunning = true;
+		
+		//TODO to change: orrible workaroung
+		Oscar.Instance.gameObject.GetComponent<Animator> ().enabled = true;
+		Camera.main.transform.parent = Oscar.Instance.transform;
+		
+	}
+}

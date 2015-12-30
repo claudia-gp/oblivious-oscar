c3ca014639338e9cf7ba19a2c422fe49fd7a8a93
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2015-12-30 13:22:19 +0100

    Fixed problem with KillingObject

--- ObliviousOscar/Assets/Scripts/KillingObject.cs
+++ ObliviousOscar/Assets/Scripts/KillingObject.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class KillingObject : MonoBehaviour
 {
 	
-	void OnTriggerEnter2D (Collider2D other)
-	{
-		if (other.CompareTag (Oscar.Tag)) {
-			OscarController.Instance.Kill ();
-		}
-	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag(Oscar.Tag))
+        {
+            OscarController.Instance.Kill();
+        }
+    }
 
 }

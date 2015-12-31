90308006f500e456af2688a337f971cdaae54f09
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2015-12-31 01:08:41 +0100

    Bugfix

--- ObliviousOscar/Assets/Scripts/TriggerFakeMagnetClass.cs
+++ ObliviousOscar/Assets/Scripts/TriggerFakeMagnetClass.cs
@@ -2,26 +2,12 @@
 
 public class TriggerFakeMagnetClass : MonoBehaviour
 {
+	public GameObject magnet;
 
-    public GameObject magnet;
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag(Oscar.Tag))
-        {
-            Destroy(magnet.GetComponent<TappableMagnet>());
-        }
-
-    }
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.CompareTag (Oscar.Tag)) {
+			Destroy (magnet.GetComponent<TappableMagnet> ());
+		}
+	}
 }

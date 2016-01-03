1d2e980e3f8c5d2a773736be50a6a726b3ce8b69
claudia-gp <claudia.gonzalez@mail.polimi.it>
2016-01-03 13:32:27 +0100

    Fixed small bug on elevator and updated prefab

--- ObliviousOscar/Assets/Scripts/CameraFollowOscar.cs
+++ ObliviousOscar/Assets/Scripts/CameraFollowOscar.cs
@@ -2,11 +2,14 @@
 
 public class CameraFollowOscar : MonoBehaviour
 {
+	public GameObject button;
+
 	void OnTriggerEnter2D (Collider2D coll)
 	{
 		if (Oscar.IsOscar (coll.gameObject)) {
 			Camera.main.transform.SetParent (Oscar.Instance.transform);	
 			OscarController.Instance.StopOscarAndSayHi ();
+			button.GetComponent<BoxCollider2D> ().enabled = true;
 		}
 	}
 }

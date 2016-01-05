0c35d783f62bab82f2e990a1268dd16d1e4a38fc
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2016-01-05 20:20:03 +0100

    Added Uphill script

---
+++ ObliviousOscar/Assets/Uphill.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Uphill : MonoBehaviour
+{
+	float initialGravity, initialMass, initialAngularDrag;
+	Rigidbody2D rb;
+
+	void Start ()
+	{
+		rb = Oscar.Instance.RigidBody2D;
+	}
+
+	void OnCollisionEnter2D (Collision2D coll)
+	{
+		if (Oscar.IsOscar (coll.gameObject)) {
+			initialGravity = rb.gravityScale;
+			initialMass = rb.mass;
+			initialAngularDrag = rb.angularDrag;
+
+			rb.gravityScale = rb.mass = rb.angularDrag = 0f;
+		}
+	}
+
+	void OnCollisionExit2D (Collision2D coll)
+	{
+		if (Oscar.IsOscar (coll.gameObject)) {
+			rb.gravityScale = initialGravity;
+			rb.mass = initialMass;
+			rb.angularDrag = initialAngularDrag;
+		}
+	}
+
+}

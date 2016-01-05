94f3b705aca8aa86d2d8816fc34892c6505f6259
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2016-01-05 20:34:05 +0100

    Better running uphill

--- ObliviousOscar/Assets/Scripts/Oscar.cs
+++ ObliviousOscar/Assets/Scripts/Oscar.cs
@@ -2,105 +2,122 @@
 
 public class Oscar : UnitySingleton<Oscar>
 {
-	public const string Tag = "Oscar";
+    const float Speed = 3f;
+    const float UphillSpeed = 2f;
+
+    public const string Tag = "Oscar";
+
+    public Sprite finalSprite;
+
+    public bool IsRunning{ get; set; }
+
+    public bool IsUphill{ get; set; }
+
+    public bool IsAnimationEnabled
+    {
+        get { return animator.enabled; }
+        set { animator.enabled = value; }
+    }
+
+    public Sprite Sprite
+    {
+        get { return spriteRenderer.sprite; }
+        set { spriteRenderer.sprite = value; }
+    }
+
+    public OscarState State
+    {
+        get{ return new OscarState(position: transform.position, direction: Direction, cameraPosition: Camera.main.transform.position); }
+        set
+        {
+            Direction = value.Direction;
+            if (Direction != transform.right)
+            {
+                spriteRenderer.flipX = !spriteRenderer.flipX;
+            }
+            transform.position = value.Position;
+            Camera.main.transform.position = value.CameraPosition;
+        }
+    }
+
+    public Sprite InitialSprite{ get; private set; }
+
+    public Rigidbody2D RigidBody2D{ get; private set; }
+
+    public Vector3 Direction{ get; private set; }
+
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    protected new void Awake()
+    {
+        base.Awake();
 	
-	public const float Speed = 3f;
+        IsRunning = true;
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        RigidBody2D = GetComponent<Rigidbody2D>();
+        InitialSprite = Sprite;
 
-	public Sprite finalSprite;
+        if (SavePointsManager.HasLatestState)
+        {
+            State = SavePointsManager.LatestState;
+        }
+        else
+        {
+            Direction = transform.right;
 
-	public bool IsRunning{ get; set; }
+            SavePointsManager.InitialState = State;
+            SavePointsManager.LatestState = State;
+        }
+    }
 
-	public bool IsAnimationEnabled {
-		get { return animator.enabled; }
-		set { animator.enabled = value; }
-	}
+    void Update()
+    {
+        if (IsRunning)
+        {
+            transform.position += Direction * Time.deltaTime * Speed;
+            if (IsUphill)
+            {
+                transform.position += transform.up * Time.deltaTime * UphillSpeed;
+            }
+        }
+    }
 
-	public Sprite Sprite {
-		get { return spriteRenderer.sprite; }
-		set { spriteRenderer.sprite = value; }
-	}
+    public void UpdateLatestState()
+    {
+        SavePointsManager.LatestState = State;
+    }
 
-	public OscarState State {
-		get{ return new OscarState (position: transform.position, direction: Direction, cameraPosition: Camera.main.transform.position); }
-		set {
-			Direction = value.Direction;
-			if (Direction != transform.right) {
-				spriteRenderer.flipX = !spriteRenderer.flipX;
-			}
-			transform.position = value.Position;
-			Camera.main.transform.position = value.CameraPosition;
-		}
-	}
+    public void ResetToInitialState()
+    {
+        SavePointsManager.LatestState = SavePointsManager.InitialState;
+    }
 
-	public Sprite InitialSprite{ get; private set; }
+    public void FlipDirection()
+    {
+        Direction *= -1;
+        spriteRenderer.flipX = !spriteRenderer.flipX;
+    }
 
-	public Rigidbody2D RigidBody2D{ get; private set; }
+    public static bool IsOscar(GameObject go)
+    {
+        return go.CompareTag(Tag);
+    }
 
-	public Vector3 Direction{ get; private set; }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.GetComponent<StopsOscar>())
+        {
+            IsRunning = false;
+        }
+    }
 
-	Animator animator;
-	SpriteRenderer spriteRenderer;
-
-	protected new void Awake ()
-	{
-		base.Awake ();
-	
-		IsRunning = true;
-		animator = GetComponent<Animator> ();
-		spriteRenderer = GetComponent<SpriteRenderer> ();
-		RigidBody2D = GetComponent<Rigidbody2D> ();
-		InitialSprite = Sprite;
-
-		if (SavePointsManager.HasLatestState) {
-			State = SavePointsManager.LatestState;
-		} else {
-			Direction = transform.right;
-
-			SavePointsManager.InitialState = State;
-			SavePointsManager.LatestState = State;
-		}
-	}
-
-	public void UpdateLatestState ()
-	{
-		SavePointsManager.LatestState = State;
-	}
-
-	public void ResetToInitialState ()
-	{
-		SavePointsManager.LatestState = SavePointsManager.InitialState;
-	}
-
-	public void FlipDirection ()
-	{
-		Direction *= -1;
-		spriteRenderer.flipX = !spriteRenderer.flipX;
-	}
-
-	public static bool IsOscar (GameObject go)
-	{
-		return go.CompareTag (Tag);
-	}
-
-	void Update ()
-	{
-		if (IsRunning) {
-			transform.position += Direction * Time.deltaTime * Speed;
-		}
-	}
-
-	void OnCollisionEnter2D (Collision2D coll)
-	{
-		if (coll.gameObject.GetComponent<StopsOscar> ()) {
-			IsRunning = false;
-		}
-	}
-
-	void OnCollisionExit2D (Collision2D coll)
-	{
-		if (coll.gameObject.GetComponent<StopsOscar> ()) {
-			IsRunning = true;
-		}
-	}
-		
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        if (coll.gameObject.GetComponent<StopsOscar>())
+        {
+            IsRunning = true;
+        }
+    }
 }
---
+++ ObliviousOscar/Assets/Scripts/Uphill.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Uphill : MonoBehaviour
+{
+    float initialGravity;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = Oscar.Instance.RigidBody2D;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (Oscar.IsOscar(coll.gameObject))
+        {
+            Oscar.Instance.IsUphill = true;
+            initialGravity = rb.gravityScale;
+            rb.gravityScale = 0f;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        if (Oscar.IsOscar(coll.gameObject))
+        {
+            Oscar.Instance.IsUphill = false;
+            rb.gravityScale = initialGravity;
+        }
+    }
+
+}

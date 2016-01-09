86c2f90ba3ca06102ab93110448ecf21568f7bdb
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2016-01-09 23:27:00 +0100

    Added camera script to better adjust to aspect ratio changes + added background image and camera

---
+++ ObliviousOscar/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraController : MonoBehaviour
+{
+	public Transform background;
+
+	const float AspectRatioConstant = 9f;
+
+	Camera cam;
+
+	void Awake ()
+	{
+		cam = GetComponent<Camera> ();
+	}
+
+	void OnRenderObject ()
+	{
+		cam.orthographicSize = AspectRatioConstant / cam.aspect;
+	}
+}

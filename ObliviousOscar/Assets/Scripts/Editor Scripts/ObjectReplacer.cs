d4621d4580652a1f057f6913642e9ad59e3aefd5
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2016-01-15 18:36:40 +0100

    Added new ground sprites and prefab

---
+++ ObliviousOscar/Assets/Scripts/Editor Scripts/ObjectReplacer.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class ObjectReplacer : MonoBehaviour
+{
+	public bool ReplaceObject;
+	public string OldObjectName = "grassCenter ";
+
+	public GameObject[] NewObjects;
+	public float[] Probabilities;
+
+	void Update ()
+	{
+		if (ReplaceObject) {
+			ReplaceObject = false;
+
+			if (Probabilities.Length != NewObjects.Length) {
+				if (Probabilities.Length != 0) {
+					Debug.Log ("Wrong probabilities length");
+					return;
+				}
+				Probabilities = new float[NewObjects.Length];
+				for (int i = 0; i < NewObjects.Length; i++) {
+					Probabilities[i] = 1f / NewObjects.Length;
+				}
+			}
+
+			GameObject[] objects = FindObjectsOfType<GameObject> ();
+			foreach (var oldGo in objects) {
+				if (oldGo.name.StartsWith (OldObjectName)) {
+					GameObject prefab = choosePrefab ();
+					GameObject newGo = PrefabUtility.InstantiatePrefab (prefab) as GameObject;
+					newGo.transform.position = oldGo.transform.position;
+					newGo.transform.parent = oldGo.transform.parent;
+					DestroyImmediate (oldGo);
+				}
+			}
+		}
+	}
+
+	GameObject choosePrefab ()
+	{
+		Debug.Assert (NewObjects.Length == Probabilities.Length);
+		int n = NewObjects.Length;
+
+		float[] probLevel = new float[n];
+		float accum = 0f;
+		for (int i = 0; i < n; i++) {
+			accum += Probabilities[i];
+			probLevel[i] = accum;
+		}
+
+		float random = Random.value;
+		Debug.Log ("Random: " + random);
+		for (int i = 0; i < n; i++) {
+			if (probLevel[i] > random) {
+				return NewObjects[i];
+			}
+		}
+
+		Debug.LogError ("I shouldn't reach this code");
+		return null;
+	}
+}
--- ObliviousOscar/Assets/Scripts/Oscar.cs
+++ ObliviousOscar/Assets/Scripts/Oscar.cs
@@ -2,106 +2,95 @@
 
 public class Oscar : UnitySingleton<Oscar>
 {
-    const float Speed = 3f;
-    const float UphillSpeed = 2f;
+	const float Speed = 3f;
+	const float UphillSpeed = 2f;
 
-    public Sprite finalSprite;
+	public Sprite finalSprite;
 
-    
-    public bool IsRunning { get; set; }
+	public bool IsRunning { get; set; }
 
-    public bool IsUphill { get; set; }
+	public bool IsUphill { get; set; }
 
-    public bool IsAnimationEnabled
-    {
-        get { return animator.enabled; }
-        set { animator.enabled = value; }
-    }
+	public bool IsAnimationEnabled {
+		get { return animator.enabled; }
+		set { animator.enabled = value; }
+	}
 
-    public Sprite Sprite
-    {
-        get { return spriteRenderer.sprite; }
-        set { spriteRenderer.sprite = value; }
-    }
+	public Sprite Sprite {
+		get { return spriteRenderer.sprite; }
+		set { spriteRenderer.sprite = value; }
+	}
 
-    public OscarState State
-    {
-        get { return new OscarState(position: transform.position, direction: Direction, cameraPosition: Camera.main.transform.position); }
-        set
-        {
-            Direction = value.Direction;
-            if (Direction != transform.right)
-            {
-                spriteRenderer.flipX = !spriteRenderer.flipX;
-            }
-            transform.position = value.Position;
-            Camera.main.transform.position = value.CameraPosition;
-        }
-    }
+	public OscarState State {
+		get { return new OscarState (position: transform.position, direction: Direction, cameraPosition: Camera.main.transform.position); }
+		set {
+			Direction = value.Direction;
+			if (Direction != transform.right) {
+				spriteRenderer.flipX = !spriteRenderer.flipX;
+			}
+			transform.position = value.Position;
+			Camera.main.transform.position = value.CameraPosition;
+		}
+	}
 
-    public Sprite InitialSprite { get; private set; }
+	public Sprite InitialSprite { get; private set; }
 
-    public Rigidbody2D RigidBody2D { get; private set; }
+	public Rigidbody2D RigidBody2D { get; private set; }
 
-    public Vector3 Direction { get; private set; }
+	public Vector3 Direction { get; private set; }
 
-    Animator animator;
-    SpriteRenderer spriteRenderer;
+	Animator animator;
+	SpriteRenderer spriteRenderer;
 
-    protected new void Awake()
-    {
-        base.Awake();
+	protected new void Awake ()
+	{
+		base.Awake ();
 
-        IsRunning = true;
-        animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        RigidBody2D = GetComponent<Rigidbody2D>();
-        InitialSprite = Sprite;
+		IsRunning = true;
+		animator = GetComponent<Animator> ();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		RigidBody2D = GetComponent<Rigidbody2D> ();
+		InitialSprite = Sprite;
 
-        if (SavePointsManager.HasLatestState)
-        {
-            State = SavePointsManager.LatestState;
-        }
-        else
-        {
-            Direction = transform.right;
+		if (SavePointsManager.HasLatestState) {
+			State = SavePointsManager.LatestState;
+		} else {
+			Direction = transform.right;
 
-            SavePointsManager.InitialState = State;
-            SavePointsManager.LatestState = State;
-        }
-    }
+			SavePointsManager.InitialState = State;
+			SavePointsManager.LatestState = State;
+		}
+	}
 
-    void Update()
-    {
-        if (IsRunning)
-        {
-            transform.position += Direction * Time.deltaTime * Speed;
-            if (IsUphill)
-            {
-                transform.position += transform.up * Time.deltaTime * UphillSpeed;
-            }
-        }
-    }
+	void Update ()
+	{
+		if (IsRunning) {
+			transform.position += Direction * Time.deltaTime * Speed;
+			if (IsUphill) {
+				transform.position += transform.up * Time.deltaTime * UphillSpeed;
+			}
+		}
+	}
 
-    public void UpdateLatestState()
-    {
-        SavePointsManager.LatestState = State;
-    }
+	public void UpdateLatestState ()
+	{
+		SavePointsManager.LatestState = State;
+	}
 
-    public void ResetToInitialState()
-    {
-        SavePointsManager.LatestState = SavePointsManager.InitialState;
-    }
+	public void ResetToInitialState ()
+	{
+		SavePointsManager.LatestState = SavePointsManager.InitialState;
+	}
 
-    public void FlipDirection()
-    {
-        Direction *= -1;
-        spriteRenderer.flipX = !spriteRenderer.flipX;
-    }
-    
-    public void SetIdle(bool isIdle)
-    {
-        IsRunning = !isIdle;
-        animator.SetBool("IsRunning", !isIdle);
-    }
+	public void FlipDirection ()
+	{
+		Direction *= -1;
+		spriteRenderer.flipX = !spriteRenderer.flipX;
+	}
+
+	public void SetIdle (bool isIdle)
+	{
+		IsRunning = !isIdle;
+		animator.SetBool ("IsRunning", !isIdle);
+	}
 }

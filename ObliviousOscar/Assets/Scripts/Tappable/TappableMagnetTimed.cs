b9ace89ad89102558e1529fab7939a427a12629b
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2016-01-16 18:12:22 +0100

    Added animation to the timed magnet, moved Oscar animation in the animation folder

--- ObliviousOscar/Assets/Scripts/Tappable/TappableMagnetTimed.cs
+++ ObliviousOscar/Assets/Scripts/Tappable/TappableMagnetTimed.cs
@@ -1,27 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TappableMagnetTimed : Tappable {
+public class TappableMagnetTimed : Tappable
+{
 
 	public float initDistance;
 	public float delay = 0.5f;
 	bool isActivated = false;
 	DistanceJoint2D dj;
+	Animator animator;
 
-	void Awake (){
+	void Awake ()
+	{
 		dj = GetComponent<DistanceJoint2D> ();
+		animator = GetComponent<Animator> ();
 	}
 
-	public override void OnClick ()	{
+	public override void OnClick ()
+	{
 		if (isActivated == false) {
 			dj.distance = 0;
 			isActivated = true;
-			StartCoroutine (waitAndFall ());
+			StartCoroutine (WaitAndFall ());
+			animator.SetFloat ("SpeedMultiplier", 1f / delay);
+			animator.enabled = true;
+		
 		} 
 
 	}
 
-	IEnumerator waitAndFall() {
+	IEnumerator WaitAndFall ()
+	{
 		yield return new WaitForSeconds (delay);
 		dj.distance = initDistance;
 		isActivated = false;

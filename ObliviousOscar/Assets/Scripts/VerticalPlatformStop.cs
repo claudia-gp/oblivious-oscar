38d0d0824ce833ecaaa2c04efb6082596b116871
claudia-gp <claudia.gonzalez@mail.polimi.it>
2016-01-17 11:21:28 +0100

    Made changes to vertical platforms & two levels

---
+++ ObliviousOscar/Assets/Scripts/VerticalPlatformStop.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerticalPlatformStop : MonoBehaviour {
+
+	public GameObject plat;
+
+	void OnCollisionEnter2D(Collider2D coll)
+	{
+		if (coll.gameObject == plat) {
+			plat.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezePositionY;
+		}
+	}
+}

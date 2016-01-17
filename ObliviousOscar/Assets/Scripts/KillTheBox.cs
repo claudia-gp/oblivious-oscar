6d78af15dc6cd1f5c9da779dd17046cd61f3b5da
claudia-gp <claudia.gonzalez@mail.polimi.it>
2016-01-17 16:27:27 +0100

    changes to 2.2

---
+++ ObliviousOscar/Assets/Scripts/KillTheBox.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillTheBox : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D box){
+		Destroy (box.gameObject.transform.parent.gameObject);
+	}
+}

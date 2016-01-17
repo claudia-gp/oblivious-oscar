73b2159e715723b933c2b9376c6c5f58dfc586ef
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2016-01-17 21:46:06 +0100

    Removed savepoint script

--- ObliviousOscar/Assets/Scripts/SavePoint.cs
+++ ObliviousOscar/Assets/Scripts/SavePoint.cs
@@ -1,14 +1,7 @@
 ﻿public class SavePoint : OscarEnterDetecter
 {
-	bool wasActivated;
-
 	protected override void OnOscarEnter ()
 	{
-		if (!wasActivated) {
-			wasActivated = true;
-			SoundManager.Instance.Play (SoundManager.Instance.SavePoint);
-		}
-
 		Oscar.Instance.UpdateLatestState ();
 	}
 }

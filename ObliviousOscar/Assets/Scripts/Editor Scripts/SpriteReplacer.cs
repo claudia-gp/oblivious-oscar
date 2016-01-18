673fb0ea59c4c0fa18019b47a368d9d8e081edda
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2016-01-18 14:11:25 +0100

    Updated half platform sprites

---
+++ ObliviousOscar/Assets/Scripts/Editor Scripts/SpriteReplacer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class SpriteReplacer : MonoBehaviour
+{
+	public bool replace;
+
+	public string leftName = "grassHalfLeft", midName = "grassHalfMid", rightName = "grassHalfRight";
+	public Sprite leftSprite, midSprite, rigthSprite;
+
+
+	void Update ()
+	{
+		if (replace) {
+			replace = false;
+
+			SpriteRenderer[] all = FindObjectsOfType<SpriteRenderer> ();
+			foreach (var sr in all) {
+				try {
+					string name = sr.sprite.name;
+					if (name == leftName) {
+						sr.sprite = leftSprite;
+					} else if (name == midName) {
+						sr.sprite = midSprite;
+					} else if (name == rightName) {
+						sr.sprite = rigthSprite;
+					}
+				} catch (System.Exception ex) {
+					Debug.Log ("Problem " + ex);
+				}
+			}
+
+			DestroyImmediate (gameObject);
+		}
+	}
+}

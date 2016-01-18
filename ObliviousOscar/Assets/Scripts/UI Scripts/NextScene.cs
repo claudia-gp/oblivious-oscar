99979de104187c76397c1c778d5ad6546c57d3be
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2016-01-18 23:24:33 +0100

    Fixed bug in next scene after winning

--- ObliviousOscar/Assets/Scripts/UI Scripts/NextScene.cs
+++ ObliviousOscar/Assets/Scripts/UI Scripts/NextScene.cs
@@ -5,7 +5,7 @@ public class NextScene : ChangeSceneInteraction
 	public override string StageToLoadOnClick {
 		get {
 			var levelToLoad = (LevelManager.CurrentIndex + 1) % LevelManager.LevelCount;
-
+			LevelManager.ResetLevel ();
 			return SceneManager.GetLevelNameByIndex (levelToLoad);
 		}
 		set {

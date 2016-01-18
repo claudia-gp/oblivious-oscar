955775bbdb9d96c828d884dd27b0dc1da5fa0721
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2016-01-18 19:35:42 +0100

    Fixed bug that caused more lives to be taken off after a kill

--- ObliviousOscar/Assets/Scripts/KillingObject.cs
+++ ObliviousOscar/Assets/Scripts/KillingObject.cs
@@ -1,7 +1,15 @@
+using System;
+
 public class KillingObject : OscarEnterDetecter
 {
-    protected override void OnOscarEnter()
-    {
-        OscarController.Instance.Kill();
-    }
+	static DateTime lastTime;
+	static readonly TimeSpan MinTimeBetweenKills = new TimeSpan (0, 0, 0, 0, 500);
+
+	protected override void OnOscarEnter ()
+	{
+		if (DateTime.Now - lastTime > MinTimeBetweenKills) {
+			lastTime = DateTime.Now;
+			OscarController.Instance.Kill ();
+		}
+	}
 }

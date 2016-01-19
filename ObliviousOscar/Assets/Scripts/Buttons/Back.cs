0f671f6dd290030333cc1f2df823c75fd8bd7c51
pedrogm <pekeott@gmail.com>
2016-01-19 12:04:10 +0100

    buttons changed on pause panel

--- ObliviousOscar/Assets/Scripts/Buttons/Back.cs
+++ ObliviousOscar/Assets/Scripts/Buttons/Back.cs
@@ -3,13 +3,15 @@
 public class Back : MonoBehaviour
 {
 	public bool OnIpad = false;
-
+	public GameObject PausePanel;
 	public  void LoadStage ()
 	{
+		PausePanel.SetActive (false);
 		if (OnIpad) {
 			LevelManager.Load ("Main Menu 1");
 		} else {
 			LevelManager.Load ("Main Menu");
+		
 		}
 	}
 }

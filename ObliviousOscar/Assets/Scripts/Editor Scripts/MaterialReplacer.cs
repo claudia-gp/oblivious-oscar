28e0c15aa79b26e9caf5d891562551d638132b6d
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2016-01-19 03:27:37 +0100

    Material replacer added

---
+++ ObliviousOscar/Assets/Scripts/Editor Scripts/MaterialReplacer.cs
@@ -0,0 +1,35 @@
+﻿#if UNITY_EDITOR
+
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MaterialReplacer : MonoBehaviour
+{
+	public bool replace;
+
+	public Material material;
+
+	void Update ()
+	{
+		if (replace) {
+			replace = false;
+
+			SpriteRenderer[] all = FindObjectsOfType<SpriteRenderer> ();
+			foreach (var sr in all) {
+				try {
+					sr.material = material;
+				} catch (System.Exception ex) {
+					Debug.Log ("Problem " + ex);
+				}
+			}
+
+			DestroyImmediate (gameObject);
+		}
+	}
+}
+
+#else
+public class MaterialReplacer : UnityEngine.MonoBehaviour
+{
+}
+#endif

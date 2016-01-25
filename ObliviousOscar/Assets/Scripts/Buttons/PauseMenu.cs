ac8eb8b07aa1960cdf99f98e55a8c9d9ef8ae45f
pedrogm <pekeott@gmail.com>
2016-01-25 20:24:41 +0100

    exit,pause and continue merget into pause menu

--- ObliviousOscar/Assets/Scripts/Buttons/PauseMenu.cs
+++ ObliviousOscar/Assets/Scripts/Buttons/PauseMenu.cs
@@ -6,20 +6,52 @@ public class PauseMenu : MonoBehaviour
 	public GameObject Oscar;
 	public GameObject ButtonPanel;
 	public GameObject HeartsPanel;
-	public GameObject PauseMenuImage;
+	public GameObject PauseMenuImage,PauseMenuImage2;
 	public GameObject BackgroundImage;
 	public GameObject LevelContainer;
+	public GameObject PauseButton;
+	public GameObject oscar;
 
-    public void OnClick()
+
+    public void Pause()
     {
 		PausePanel.SetActive(true);
 		Oscar.SetActive (false);
 		ButtonPanel.SetActive (false);
 		HeartsPanel.SetActive(false);
 		PauseMenuImage.SetActive(true);
+		PauseMenuImage2.SetActive(true);
 		BackgroundImage.SetActive(false);
 		LevelContainer.SetActive(false);
     }
+
+	public  void Exit ()
+	{
+
+		HeartsPanel.SetActive(true);
+		PauseMenuImage.SetActive(false);
+		PauseMenuImage2.SetActive(false);
+		BackgroundImage.SetActive(true);
+		LevelContainer.SetActive(true);
+		PauseButton.SetActive (true);
+		PausePanel.SetActive (false);
+		oscar.SetActive(true);
+		LevelManager.Load ("Main Menu");
+
+
+	}
+
+	public void Continue () {
+
+		PausePanel.SetActive(false);
+		Oscar.SetActive(true);
+		ButtonPanel.SetActive (true);
+		HeartsPanel.SetActive (true);
+		BackgroundImage.SetActive(true);
+		PauseMenuImage.SetActive(false);
+		PauseMenuImage2.SetActive(false);
+		LevelContainer.SetActive (true);
+	}
 
 }
 

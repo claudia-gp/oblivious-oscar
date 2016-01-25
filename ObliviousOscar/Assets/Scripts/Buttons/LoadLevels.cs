0ae4b9497c960501263d768bc5d4cae2b2ec26a8
pedrogm <pekeott@gmail.com>
2016-01-25 17:52:29 +0100

    levels scripts in LoadLevels

---
+++ ObliviousOscar/Assets/Scripts/Buttons/LoadLevels.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevels : MonoBehaviour {
+
+	public void Tutorial (){
+		LevelManager.Load ("Level 1.1-Tutorial");
+	}
+	public void Level1_2 (){
+		LevelManager.Load ("Level 1.2");
+	}
+	public void Level1_3 (){
+		LevelManager.Load ("Level 1.3");
+	}
+	public void Level1_4 (){
+		LevelManager.Load ("Level 1.4");
+	}
+	public void Level1_5 (){
+		LevelManager.Load ("Level 1.5");
+	}
+	public void Level2_1 (){
+		LevelManager.Load ("Level 2.1");
+	}
+	public void Level2_2 (){
+		LevelManager.Load ("Level 2.2");
+	}
+	public void Level2_3 (){
+		LevelManager.Load ("Level 2.3");
+	}
+
+}

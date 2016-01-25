9de2638733713d1e9bb415bd2286963068ac3622
claudia-gp <claudia.gonzalez@mail.polimi.it>
2016-01-25 18:42:57 +0100

    Organized sprites and options panel in main menu (incomplete)

---
+++ ObliviousOscar/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionsMenu : MonoBehaviour {
+
+	public GameObject OptionsPanel;
+	public GameObject PlayButton;
+	public GameObject LevelsButton;
+
+	public void OnClick()
+	{
+		OptionsPanel.SetActive(true);
+		PlayButton.GetComponent<Button> ().enabled = false;
+		LevelsButton.GetComponent<Button> ().enabled = false;
+	}
+
+
+}

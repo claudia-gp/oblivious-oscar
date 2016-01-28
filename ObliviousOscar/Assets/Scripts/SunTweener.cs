1871bc8a9b9dd6bdac51951f4654c23ed32e9cc9
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2016-01-28 18:20:38 +0100

    Better sun

--- ObliviousOscar/Assets/Scripts/SunTweener.cs
+++ ObliviousOscar/Assets/Scripts/SunTweener.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
 using DG.Tweening;
+using System.Collections;
 
 public class SunTweener : MonoBehaviour
 {
-	const float duration = 3f;
-	const float scale = 1.3f;
+	const float duration = 0.5f;
+	const float scale = 1.06f;
+	const float pause = 0.7f;
+	const Ease ease = Ease.InSine;
 
 	void Start ()
 	{
-		transform.DOScale (scale, duration).OnComplete (
-			() => transform.DOScale (1f, duration).OnComplete (Start)
+		StartCoroutine (SunTweenBig ());
+	}
+
+	IEnumerator SunTweenBig ()
+	{
+		yield return new WaitForSeconds (pause);
+		transform.DOScale (scale, duration).SetEase (ease).OnComplete (
+			() => StartCoroutine (SunTweenSmall ())
+		);
+	}
+
+	IEnumerator SunTweenSmall ()
+	{
+		yield return new WaitForSeconds (0f);
+		transform.DOScale (1f, duration).SetEase (ease).OnComplete (
+			() => StartCoroutine (SunTweenBig ())
 		);
 	}
 

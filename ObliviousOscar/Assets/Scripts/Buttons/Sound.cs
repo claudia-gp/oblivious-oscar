3b3dcee5c282e38d4cdc156d6ce44fa9b56a2915
pedrogm <pekeott@gmail.com>
2016-01-28 21:17:32 +0100

    sound and music buttons on options panel work (need to be changed to only disable sound/music)

--- ObliviousOscar/Assets/Scripts/Buttons/Sound.cs
+++ ObliviousOscar/Assets/Scripts/Buttons/Sound.cs
@@ -4,24 +4,73 @@ using System.Collections;
 public class Sound : MonoBehaviour {
 
 	public GameObject SoundButtonOn; 
-	public GameObject SoundButtonOff; 
+	public GameObject SoundButtonOff;
+	public GameObject MusicButtonOn;
+	public GameObject MusicButtonOff;
 
 
 	public void SoundOn(){
 
 		//Deactivate sound	
 		SoundManager.Instance.SetMusic(false);
-		SoundButtonOff.SetActive (true);
-		SoundButtonOn.SetActive (false);
+		changeAspectOn ();
 	}
 	
 	public void SoundOff(){
 
 		//Activate Sound
 		SoundManager.Instance.SetMusic(true);
-		SoundButtonOn.SetActive(true);
-		SoundButtonOff.SetActive(false);
+		changeAspectOff();
 
 
 	}
+
+	public void OptionsSoundOn(){
+
+		//Deactivate sound	
+		SoundManager.Instance.SetMusic(false);
+		changeAspectOn ();
+	}
+
+	public void OptionsSoundOff(){
+
+		//Activate Sound
+		SoundManager.Instance.SetMusic(true);
+		changeAspectOff ();
+
+
+	}
+
+	public void OptionsMusicOn(){
+
+		//Deactivate sound	
+		SoundManager.Instance.SetMusic(false);
+		MusicButtonOff.SetActive (true);
+		MusicButtonOn.SetActive (false);
+	}
+
+	public void OptionsMusicOff(){
+
+		//Activate Sound
+		SoundManager.Instance.SetMusic(true);
+		MusicButtonOn.SetActive(true);
+		MusicButtonOff.SetActive(false);
+
+
+	}
+
+	void changeAspectOn(){
+
+		SoundButtonOff.SetActive (true);
+		SoundButtonOn.SetActive (false);
+		
+	}
+
+	void changeAspectOff(){
+
+		SoundButtonOn.SetActive(true);
+		SoundButtonOff.SetActive(false);
+		
+	}
+
 }

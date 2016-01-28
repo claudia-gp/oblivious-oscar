474fc4b241fd523897469e19a7277a8807665fd4
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2016-01-28 17:23:13 +0100

    Added sun that changes scale

---
+++ ObliviousOscar/Assets/Scripts/SunTweener.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class SunTweener : MonoBehaviour
+{
+	const float duration = 3f;
+	const float scale = 1.3f;
+
+	void Start ()
+	{
+		transform.DOScale (scale, duration).OnComplete (
+			() => transform.DOScale (1f, duration).OnComplete (Start)
+		);
+	}
+
+}

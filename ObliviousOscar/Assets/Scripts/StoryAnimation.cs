ace30ca2ada8caa77c2a2fb3b9b122451ccf4611
claudia-gp <claudia.gonzalez@mail.polimi.it>
2016-01-29 03:27:49 +0100

    Added text to pause menu and animation

--- ObliviousOscar/Assets/Scripts/StoryAnimation.cs
+++ ObliviousOscar/Assets/Scripts/StoryAnimation.cs
@@ -2,26 +2,43 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class StoryAnimation : MonoBehaviour {
+public class StoryAnimation : MonoBehaviour
+{
 
 	public Sprite[] introSequence;
+	public Text[] texts;
+	public Text initialText;
+	public Text initialText2;
+
 	int current = 0;
 	Image image;
 
-	void Awake(){
+	void Awake ()
+	{
 		image = GetComponent<Image> ();
 	}
-		
 
 	public void ChangeImage ()
 	{
-		if(current == introSequence.Length){
+		if (current == introSequence.Length) {
 			GoToLevel ();
+		} else { 
+			if (current == 0) {
+				initialText.enabled = false;
+				initialText2.enabled = false;
+			} else {
+				texts [current - 1].enabled = false;
+			}
+
+			texts [current].enabled = true;
+			image.overrideSprite = introSequence [current];
+
+			current++;
 		}
-		image.overrideSprite = introSequence[current++];
 	}
 
-	public void GoToLevel (){
+	public void GoToLevel ()
+	{
 		LevelManager.LoadAfterLoadingScreen (LevelManager.FirstLevel);
 	}
 

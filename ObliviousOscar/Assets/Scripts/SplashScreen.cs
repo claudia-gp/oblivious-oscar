a56f7fc728740e9b2b142a0891ef0086e95d36b8
Alessandro Cantini <alessandro.cantini@mail.polimi.it>
2016-02-05 18:27:00 +0100

    added splash screen

---
+++ ObliviousOscar/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+	const float splashScreenDuration = 1.5f;
+
+	void Start ()
+	{
+		StartCoroutine (LaunchGame ());
+	}
+
+	IEnumerator LaunchGame ()
+	{
+		var loading = SceneManager.LoadSceneAsync (LevelManager.MainMenu);
+		loading.allowSceneActivation = false;
+		yield return new WaitForSeconds (splashScreenDuration);
+		loading.allowSceneActivation = true;
+	}
+}

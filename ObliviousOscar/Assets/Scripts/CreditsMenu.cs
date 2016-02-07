dd52ad70d447ef4ee2e307b3d6f89dddec414a55
claudia-gp <claudia.gonzalez@mail.polimi.it>
2016-02-07 19:45:28 +0100

    Fixed menu scenes and added credits button

---
+++ ObliviousOscar/Assets/Scripts/CreditsMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsMenu : MonoBehaviour {
+	public void show ()
+	{
+		LevelManager.Load (LevelManager.Credits);
+	}
+
+	public void hide ()
+	{
+		LevelManager.Load (LevelManager.MainMenu);
+	}
+}
---
+++ ObliviousOscar/Assets/Scripts/Level elements/Triggers/cameraSetToOscar.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraSetToOscar : OscarEnterDetecter {
+
+	protected override void OnOscarEnter()
+	{
+		Camera.main.transform.SetParent (Oscar.Instance.transform); 
+	}
+}
